fed70e37aee1ab132081c804aaba7aa918e6bcbc
Christophe Van Neste <christophe.vanneste@ugent.be>
2018-01-30 20:36:05 +0100

    first classes implemented

--- ninklings/__init__.py
+++ ninklings/__init__.py
@@ -7,8 +7,8 @@ import pandas as pd
 from io import  StringIO
 import os
 
-# Set matplotlib to interactive
-plt.ion()
+# Set matplotlib to interactive mode when executed from interactive shell
+if os.sys.ps1: plt.ion()
 
 ## Biomart
 def get_biomart(atts=None):
@@ -28,58 +28,109 @@ def get_biomart(atts=None):
     data = pd.read_table(StringIO(s.content.decode()))
     return data
 
-b = get_biomart()
-b=b[b['Source of gene name']=='HGNC Symbol']
-g=b['Gene name']
-b=b[~g.duplicated()]
-#ask server to not send duplicate ids
-b['network_id'] = b['Gene stable ID'].apply(lambda x: int(x.replace('ENSG','')))
+class Netwink():
+    """
+    Ninkling's starting class.
+    Does the setup for all down stream network analyses
+    """
 
-# Load network
-# als complex netwerk -> complex als entiteit meenemen?
-# voor brute analyze best niet
-# TODO hoe gaan we daarmee om
-networktable = pd.read_table(os.path.expanduser('~/Dropbiz/Personal/Tools/ninklings/data/reactome_FI.txt'))
-networktable = networktable[networktable.Gene1.isin(b['Gene name'])]
-networktable = networktable[networktable.Gene2.isin(b['Gene name'])]
-networktable = networktable[networktable.Score >= .75]
-print('unique genes:',len(set(networktable.Gene1)|set(networktable.Gene2)))
-networkgenes = networktable[['Gene1','Gene2']]
-networkgenes.to_csv(os.path.expanduser('~/Dropbiz/Personal/Tools/ninklings/data/reactome_FI_filteredEdges.txt'),
-                    sep='\t')
+    def __init__(self,savedir,name=None,cosmicOnly=True,fullInit=True):
+        self.location = savedir
+        self.name = name if name else os.path.basename(savedir)
+        if fullInit:
+            self.check_location()
+            self.set_annotation()
+            self.load_network(cosmicOnly)
 
-cosmicgenes = set(pd.read_table(os.path.expanduser('/Users/cvneste/Dropbiz/Personal/Tools/ninklings/data/cosmic_20180125.tsv'))['Gene Symbol'])
+    def check_location(self):
+        if not os.path.exists(self.location):
+            os.mkdir(self.location)
+    
+    def set_annotation(self):
+        self.annotationFile = os.path.join(self.location, 'ensembl_annotation.tsv')
+        if os.path.exists(self.annotationFile):
+            self.annotation = pd.read_table(self.annotationFile)
+        else:
+            self.annotation = get_biomart()
+            self.annotation = self.annotation[
+                self.annotation['Source of gene name']=='HGNC Symbol'
+            ]
+            self.annotation = self.annotation[
+                ~self.annotation['Gene name'].duplicated()
+            ] #TODO ask server to not send duplicate ids
+            self.annotation['network_id'] = self.annotation['Gene stable ID'].apply(lambda x: int(x.replace('ENSG','')))
+            self.annotation.to_csv(self.annotationFile,sep='\t')
 
-reactome_FI = nx.Graph()
-networkgenes.T.apply(lambda x: reactome_FI.add_edge(x['Gene1'],x['Gene2']))
+    def load_network(self,cosmicOnly=False):
+        # als complex netwerk -> complex als entiteit meenemen?
+        # voor brute analyze best niet
+        # TODO hoe gaan we daarmee om
+        self.networkFile = os.path.join(self.location, 'reactome_FI_filteredEdges.tsv')
+        if os.path.exists(self.networkFile):
+            self.annotation = pd.read_table(self.networkFile)
+        else:
+            networktable = pd.read_table(os.path.expanduser('~/Dropbiz/Personal/Tools/ninklings/data/reactome_FI.txt'))
+            networktable = networktable[networktable.Gene1.isin(self.annotation['Gene name'])]
+            networktable = networktable[networktable.Gene2.isin(self.annotation['Gene name'])]
+            networktable = networktable[networktable.Score >= .75]
+            print('unique genes:',len(set(networktable.Gene1)|set(networktable.Gene2)))
+            self.networkgenes = networktable[['Gene1','Gene2']]
+            self.networkgenes.to_csv(self.networkFile, sep='\t')
+        # Construct network
+        self.graph = nx.Graph()
+        self.networkgenes.T.apply(lambda x: self.graph.add_edge(x['Gene1'],x['Gene2']))
+        if cosmicOnly:
+            cosmicgenes = set(pd.read_table(
+                os.path.expanduser('/Users/cvneste/Dropbiz/Personal/Tools/ninklings/data/cosmic_20180125.tsv')
+            )['Gene Symbol'])
+            self.graph = self.graph.subgraph(cosmicgenes)
+            components = {
+                comp.number_of_nodes():comp
+                for comp in nx.connected_component_subgraphs(self.graph)
+            }
+            self.graph = components[max(components)]
+        self.admatrix = nx.adjacency_matrix(self.graph).todense()
+        self.admatrix = self.admatrix.astype(np.float32)
 
-admatrix = nx.adjacency_matrix(reactome_FI)
+    def export_admatrix(self):
+        self.admatrix.tofile(os.path.join(self.location,'admatrix.tsv'), sep='\t')
 
-reactome_FI_cosmic = reactome_FI.subgraph(cosmicgenes)
+    def plot_admatrix(self):
+        plt.matshow(self.admatrix)
 
-for comp in nx.connected_component_subgraphs(reactome_FI_cosmic):
-    print(comp.number_of_nodes())
-    if comp.number_of_nodes() == 534:
-        reactome_FI_cosmic = comp
-        break
+# Kernels
+class Kernel():
+    def __init__(self,netwink):
+        self.netwink = netwink
 
-admatrix = nx.adjacency_matrix(reactome_FI_cosmic)
-admatrix = admatrix.todense()
-admatrix = admatrix.astype(np.float32)
-plt.matshow(admatrix)
+    def __repr__(self):
+        if 'computedMatrix' in vars(k):
+            return self.computedMatrix.__repr__()
 
-#laplacian exponential diffusion kernel
-degreematrix = np.diag(np.array(admatrix.sum(axis=1)).flatten())
-laplacian = degreematrix - admatrix
-alpha = 1
-expm(-alpha*np.array(laplacian)) # => wel een kernel
-admatrix.tofile('Downloads/admatrix.txt', sep='\t')
+    def compute(self):
+        raise NotImplementedError('implement in inheriting classes')
 
-#restart random walk
-restartProb = 0.1
-inversematrix = np.linalg.inv(degreematrix - ((1-restartProb)*admatrix))
-restartmatrix = inversematrix*degreematrix # => op factor na probabiliteiten => geen kernel
-plt.matshow(restartmatrix)
+    def visualize(self):
+        plt.matshow(self.computedMatrix)
+        
+## laplacian exponential diffusion kernel
+class ExponentialDiffusionKernel(Kernel):
+    def compute(self,alpha = 1):
+        from scipy.linalg import expm
+        self.degreematrix = np.diag(np.array(self.netwink.admatrix.sum(axis=1)).flatten())
+        self.laplacian = self.degreematrix - self.netwink.admatrix
+        self.computedMatrix = expm(-alpha*np.array(self.laplacian)) # => wel een kernel
+        return self
+
+## restart random walk
+class RestartRandomWalk(Kernel):
+    def compute(self,restartProb = 0.1):
+        self.degreematrix = np.diag(np.array(self.netwink.admatrix.sum(axis=1)).flatten())
+        self.inversematrix = np.linalg.inv(self.degreematrix - ((1-restartProb)*self.netwink.admatrix))
+        self.restartmatrix = self.inversematrix*self.degreematrix # => op factor na probabiliteiten => geen kernel
+        self.computedMatrix = self.restartmatrix
+        return self
+        
 #vertrekken van gen dat e.g. gemuteerd is (restartmatrix is niet symmetrisch)
 # restartmatrix -> converteren naar afstandsmatrix (1 - ) en diagonaal op 0 zetten
 # daarmee kan je clusteren -> groepen van genen in elkaars buurt
